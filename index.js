76d196cca53d8eb0197aeefebbf5bcc601848465
easauceda <easauceda@gmail.com>
2016-08-29 23:16:22 -0700

    structure for slack

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ var app = express();
 var PORT = process.env.PORT || '3000';
 
 app.get('/api/school', function(req, res){
-  res.send('This is school information!');
+  var payload = {
+    'response_type': 'in_channel',
+    'text': 'Here are some schools in that area'
+  };
+  res.json(payload);
 });
 
 app.listen(PORT, function(err){
